3847d52d48481a0219ddff9f5cf9d525838011e1
serrebx <sserre.bx@gmail.com>
2013-03-15 17:04:57 +0100

    Increased packet header len to 4 bytes.

--- lib/ifs/src/ssl_client.erl
+++ lib/ifs/src/ssl_client.erl
@@ -147,7 +147,8 @@ init([Encoder, {Key, Cert, CACert}]) ->
         {keyfile,           State#client_state.key},
         {protocol,          tlsv1},
         {active,            false}], 3000),
-	ssl:setopts(SSLSocket, [{active, once}, {packet, 2}, binary]),
+	ssl:setopts(SSLSocket, [{active, once}, {packet, 4}, binary]),
+
 	{ok, {IP, Port}} = ssl:peername(SSLSocket),
     NextState = State#client_state{
         socket          = SSLSocket,
