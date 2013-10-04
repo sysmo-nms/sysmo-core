4d9a937ee99813d11918b05cf10a1f97235677eb
ssbx <sserre.bx@gmail.com>
2013-10-04 14:34:27 +0200

    Inspectors are now mandatory to modify the probe state. Delete unused code

--- lib/tracker/src/inspectors/btracker_inspector_standard.erl
+++ lib/tracker/src/inspectors/btracker_inspector_standard.erl
@@ -1,26 +1,26 @@
 % This file is part of "Enms" (http://sourceforge.net/projects/enms/)
 % Copyright (C) 2012 <Sébastien Serre sserre.bx@gmail.com>
-% 
+%
 % Enms is a Network Management System aimed to manage and monitor SNMP
 % targets, monitor network hosts and services, provide a consistent
 % documentation system and tools to help network professionals
 % to have a wide perspective of the networks they manage.
-% 
+%
 % Enms is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
 % (at your option) any later version.
-% 
+%
 % Enms is distributed in the hope that it will be useful,
 % but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 % GNU General Public License for more details.
-% 
+%
 % You should have received a copy of the GNU General Public License
-% along with Enms.  If not, see <http://www.gnu.org/licenses/>.
+% along with Enms. If not, see <http://www.gnu.org/licenses/>.
 % @doc
-% The standard inspector. Add a configurable latency. Will prevent 
-% false positive.
+% The most simple and mandatory inspector. It set re return status of the
+% ProbeServer to the return status of the probe return.
 % @end
 -module(btracker_inspector_standard).
 -behaviour(beha_tracker_inspector).
@@ -28,29 +28,24 @@
 
 
 -export([
-    init/2, 
+    init/2,
     inspect/3,
     info/0
 ]).
 
 info() ->
-    [
-        'OK',
-        'WARNING',
-        'CRITICAL',
-        'RECOVERY'
-    ].
+    [].
 
-init(_Conf, PrState) -> 
-    {ok, PrState}.
+init(_Conf, ProbeServerState) ->
+    {ok, ProbeServerState}.
 
 % @end
-inspect(_InitS, 
-        #probe_server_state{probe = Probe} = PrState, {ok, _}) ->
-    NewProbe = Probe#probe{status = 'OK'},
-    {ok, PrState#probe_server_state{probe = NewProbe}};
-
-inspect(_InitS, 
-        #probe_server_state{probe = Probe} = PrState, {error, _}) ->
-    NewProbe = Probe#probe{status = 'CRITICAL'},
-    {ok, PrState#probe_server_state{probe = NewProbe}}.
+inspect(_InitS,
+            #probe_server_state{
+                probe = Probe
+            } = ProbeServerState,
+            #probe_return{
+                status = Status
+            }) ->
+    NewProbe = Probe#probe{status = Status},
+    {ok, ProbeServerState#probe_server_state{probe = NewProbe}}.
