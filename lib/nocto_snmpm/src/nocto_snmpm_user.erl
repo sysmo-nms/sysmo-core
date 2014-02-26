5b51c00c6c786eb984346e0d39126b0a1015c678
ssbx <sserre.bx@gmail.com>
2014-02-26 10:56:57 +0100

    cleaning up, bulk walk enhencement

--- lib/nocto_snmpm/src/nocto_snmpm_user.erl
+++ lib/nocto_snmpm/src/nocto_snmpm_user.erl
@@ -217,7 +217,7 @@ sync_walk_bulk(Agent, StartOID, LastOID, Result) ->
     end.
 
 remove_out_of_tree_OIDs(Oid, List) ->
-    lists:filter(fun({_,X,_,_,_}) ->
+    lists:takewhile(fun({_,X,_,_,_}) ->
         still_in_tree(Oid, X)
     end, List).
 
@@ -230,73 +230,3 @@ still_in_tree([TH|TreeOid] , [OH|Oid]) when TH == OH ->
 still_in_tree(_,_) ->
     false.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% -record(interface, {
-%     if_index            = 0             :: integer(),
-%     if_descr            = ""            :: string(),
-%     if_type             = 0             :: integer(),
-%     if_mtu              = 0             :: integer(),
-%     if_speed            = 0             :: integer(),
-%     if_phys_address     = []            :: [integer()]
-% }).
- 
-% -record(state, {
-%     agents              = []            :: [#agent{}]
-% }).
-
-% initialize_agent(Agent) ->
-%     Reply = snmpm:sync_get(?SNMP_USER, Agent, 
-%         [?SYS_NAME_OID, ?SYS_SERVICES_OID, ?IF_NUMBER_OID]
-%     ),
-%     case Reply of
-%         {ok, {noError, _, Rep}, _} ->
-%             {_,_,_,SysName,_}   = lists:keyfind(?SYS_NAME_OID,      2, Rep),
-%             {_,_,_,SysServ,_}   = lists:keyfind(?SYS_SERVICES_OID,  2, Rep),
-%             {_,_,_,IfNumber,_}  = lists:keyfind(?IF_NUMBER_OID,     2, Rep),
-%             Interfaces = initialize_interfaces(IfNumber, Agent),
-%             #agent{
-%                 name            = Agent,
-%                 sys_name        = SysName,
-%                 sys_services    = SysServ,
-%                 if_number       = IfNumber,
-%                 interfaces      = Interfaces
-%             };
-%         _ ->
-%             ?LOG({reply_error, Agent, Reply})
-%     end.
-% 
-% initialize_interfaces(IfNumber, Agent) ->
-%     {ok, {noError, _, IfEntrysRem0}, _} = snmpm:sync_get_bulk(
-%         ?SNMP_USER, Agent, 0, IfNumber * 6,[
-%             [1,3,6,1,2,1,2,2,1]
-%         ]
-%     ),
-%     {IfIndexes, IfEntrysRem1}  = lists:split(IfNumber, IfEntrysRem0),
-%     {_IfDescr,   IfEntrysRem2}  = lists:split(IfNumber, IfEntrysRem1),
-%     {_IfType,    IfEntrysRem3}  = lists:split(IfNumber, IfEntrysRem2),
-%     {_IfMtu,     IfEntrysRem4}  = lists:split(IfNumber, IfEntrysRem3),
-%     {_IfSpeed,   _IfPhysAddress} = lists:split(IfNumber, IfEntrysRem4),
-%     generate_if_record(IfIndexes, IfEntrysRem1).
-%     %?LOG({IfIndexes, IfDescr, IfType, IfMtu, IfSpeed, IfPhysAddress}).
-% 
-% generate_if_record(IfIndexes, IfEntrys) ->
-%     generate_if_record([], IfIndexes, IfEntrys).
-% generate_if_record(Result, [], _) ->
-%     Result;
-% generate_if_record(Result, [IfIndex | IfIndexes], IfEntrys) ->
-%     {_,_,_,Index,  _}  = IfIndex,
-%     {_,_,_,IfDescr,_} = lists:keyfind([1,3,6,1,2,1,2,2,1,2,Index], 2, IfEntrys),
-%     {_,_,_,IfType, _} = lists:keyfind([1,3,6,1,2,1,2,2,1,3,Index], 2, IfEntrys),
-%     {_,_,_,IfMtu,  _} = lists:keyfind([1,3,6,1,2,1,2,2,1,4,Index], 2, IfEntrys),
-%     {_,_,_,IfSpeed,_} = lists:keyfind([1,3,6,1,2,1,2,2,1,5,Index], 2, IfEntrys),
-%     {_,_,_,IfPhysAddress,_} = lists:keyfind([1,3,6,1,2,1,2,2,1,6,Index], 2, IfEntrys),
-% 
-%     If = #interface{
-%         if_index        = Index,
-%         if_descr        = IfDescr,
-%         if_type         = IfType,
-%         if_mtu          = IfMtu,
-%         if_speed        = IfSpeed,
-%         if_phys_address = IfPhysAddress
-%     },
-%     generate_if_record([If | Result], IfIndexes, IfEntrys).
