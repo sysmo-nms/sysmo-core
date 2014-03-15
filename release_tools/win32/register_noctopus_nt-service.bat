6ba6dc1210343434d9b96ecc323d5c4591368163
ssbx <sserre.bx@gmail.com>
2014-03-15 01:34:31 -0700

    working service registration win32

---
+++ release_tools/win32/register_noctopus_nt-service.bat
@@ -0,0 +1,9 @@
+"C:\Documents and Settings\seb\Desktop\testInstall\noctopus-server\erts-5.10.4\bin\erlsrv.exe" ^
+    add "Noctopus-NMS" -comment "Noctopus server service" ^
+    -stopaction "init:stop()." ^
+    -onfail restart ^
+    -machine "C:\Documents and Settings\seb\Desktop\testInstall\noctopus-server\erts-5.10.4\bin\erl.exe" ^
+    -priority high ^
+    -args "-boot \"C:/Documents and Settings/seb/Desktop/testInstall/noctopus-server/releases/0.2.1/start\" -config \"C:/Documents and Settings/seb/Desktop/testInstall/noctopus-server/releases/0.2.1/sys\""
+
+"C:\Documents and Settings\seb\Desktop\testInstall\noctopus-server\erts-5.10.4\bin\erlsrv.exe" start "Noctopus-NMS"
