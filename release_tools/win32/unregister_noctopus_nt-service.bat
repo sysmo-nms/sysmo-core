0f9147887b586e8e510080f2402358787e00203b
ssbx <sserre.bx@gmail.com>
2014-03-17 05:40:48 -0700

    change service name to "Noctopus NMS"

--- release_tools/win32/unregister_noctopus_nt-service.bat
+++ release_tools/win32/unregister_noctopus_nt-service.bat
@@ -1,1 +1,1 @@
-"C:\Documents and Settings\seb\Desktop\testInstall\noctopus-server\erts-5.10.4\bin\erlsrv.exe" remove "Noctopus base server"
+"C:\Documents and Settings\seb\Desktop\testInstall\noctopus-server\erts-5.10.4\bin\erlsrv.exe" remove "Noctopus NMS"
