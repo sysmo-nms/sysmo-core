fbf2e7ed9746c7640d4fb1f08966f7f864454893
ssbx <sserre.bx@gmail.com>
2014-08-19 17:41:33 +0200

    ssl script modif

--- cfg/ssl/generate_test_certificate.sh
+++ cfg/ssl/generate_test_certificate.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 echo "Generating test certificate for server localhost."
+echo "Used by the supercast server."
 echo "MANDATORY: \"Common Name\" must be \"localhost\"."
-echo "Other filds can be ignored."
+echo "Other fields can be ignored."
 echo -n "Press enter to begin: "
 read
-echo "\n"
+echo 
 openssl genrsa -out key.pem 1024
 openssl req -new -key key.pem -out request.pem
 openssl x509 -req -days 30 -in request.pem -signkey key.pem -out certificate.pem
