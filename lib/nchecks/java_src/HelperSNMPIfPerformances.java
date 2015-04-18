12ec2813d77e929360ecc7f5b6423764fbd7d202
ssbx <sserre.bx@gmail.com>
2015-04-18 09:05:32 +0200

    include nchecks helpers

--- lib/nchecks/java_src/HelperSNMPIfPerformances.java
+++ lib/nchecks/java_src/HelperSNMPIfPerformances.java
@@ -19,61 +19,26 @@
  * THE SOFTWARE.
  */
 
-package io.sysmo.nchecks.modules;
+package io.sysmo.nchecks.helpers;
 
-import io.sysmo.nchecks.NChecksInterface;
+import io.sysmo.nchecks.NHelpersInterface;
 import io.sysmo.nchecks.Argument;
-import io.sysmo.nchecks.Reply;
-import io.sysmo.nchecks.Const;
 
 import java.util.Map;
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-import java.net.Socket;
-import java.net.InetSocketAddress;
-import java.net.InetAddress;
 
 
-public class CheckSNMPIfPerformances implements NChecksInterface
+public class HelperSNMPIfPerformances implements NHelpersInterface
 {
-    private String  ifSelection;
-
-    public CheckSNMPIfPerformances()
+    public HelperSNMPIfPerformances()
     {
         System.out.println("init CheckIfPerformances");
     }
 
-    public void setOpaqueData(byte[] opaqueData)
-    {
-        /* DESERIALIZATION EXAMPLE
-        ByteArrayInputStream b = new ByteArrayInputStream(opaqueData);
-        ObjectInputStream o = new ObjectInputStream(b);
-
-        Object myObject = o.readObject();
-            or beter
-        MyObjectClass = (MyObjectClass) o.readObject();
-        */
-
-        /* SERICALIZATION EXAMPLE
-        ByteArrayOutputStream b = new ByteArrayOutputStream();
-        ObjectOutputStream o = new ObjectOutputStream(b);
-        o.writeObject(myObject);
-        opaqueData = b.toByteArray();
-        */
-    }
-
     public void setConfig(Map<String,Argument> config)
     {
-        Argument ifPerfArg = config.get("if_selection");
-
-        ifSelection = ifPerfArg.getStr();
     }
 
-    public Reply execute()
+    public void execute()
     {
-        Reply reply = new Reply();
-        reply.setStatus(Const.STATUS_OK);
-        reply.setReply("IfPerTableTest... ok blabla");
-        return reply;
     }
 }
