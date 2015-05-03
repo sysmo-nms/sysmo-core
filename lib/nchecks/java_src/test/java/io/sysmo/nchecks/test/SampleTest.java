47f51931b98150844a173a8ade507d8ced19051c
ssbx <sserre.bx@gmail.com>
2015-05-03 12:41:47 +0200

    sample test add

---
+++ lib/nchecks/java_src/test/java/io/sysmo/nchecks/test/SampleTest.java
@@ -0,0 +1,36 @@
+package io.sysmo.nchecks.test;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class SampleTest {
+
+    private java.util.List emptyList;
+
+    /**
+     *      * Sets up the test fixture. 
+     *           * (Called before every test case method.)
+     *                */
+    @Before
+    public void setUp() {
+        emptyList = new java.util.ArrayList();
+    }
+
+    /**
+     *      * Tears down the test fixture. 
+     *           * (Called after every test case method.)
+     *                */
+    @After
+    public void tearDown() {
+        emptyList = null;
+    }
+
+    @Test
+    public void testSomeBehavior() {
+        assertEquals("Empty list should have 0 elements", 1, emptyList.size());
+    }
+
+    @Test(expected=IndexOutOfBoundsException.class)
+    public void testForException() {
+        Object o = emptyList.get(0);
+    }
+}
