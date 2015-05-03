9e5e17c3cd7187936a72f824854913ea5f0a296f
ssbx <sserre.bx@gmail.com>
2015-05-03 23:30:52 +0200

    forgot to rename snmpman src directory

--- helpers/getJavaDist.sh
+++ helpers/getJavaDist.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-rm -rf rel/sysmo/java_dist
-mkdir rel/sysmo/java_dist
+rm -rf rel/sysmo/java_apps
+mkdir rel/sysmo/java_apps
 for app in errd4j snmpman nchecks equartz; do
-    cp -R java/${app}/build/install/${app} rel/sysmo/java_dist/
+    cp -R java/${app}/build/install/${app} rel/sysmo/java_apps/
 done
