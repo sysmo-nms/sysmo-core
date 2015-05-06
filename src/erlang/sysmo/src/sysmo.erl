60698e9110d92725190110a36ddfdbce98210561
ssbx <sserre.bx@gmail.com>
2015-05-06 14:06:47 +0200

    add wide config to sysmo

---
+++ src/erlang/sysmo/src/sysmo.erl
@@ -0,0 +1,31 @@
+-module(sysmo).
+-export([
+    get_log_dir/0,
+    get_conf_dir/0,
+    get_data_dir/0,
+    get_java_dir/0,
+
+    get_java_bin_prefix/0
+]).
+
+get_log_dir() ->
+    {ok, Dir} = application:get_env(sysmo, log_dir),
+    Dir.
+
+get_conf_dir() ->
+    {ok, Dir} = application:get_env(sysmo, conf_dir),
+    Dir.
+
+get_data_dir() ->
+    {ok, Dir} = application:get_env(sysmo, data_dir),
+    Dir.
+
+get_java_dir() ->
+    {ok, Dir} = application:get_env(sysmo, java_dir),
+    Dir.
+
+get_java_bin_prefix() ->
+    case os:type() of
+        {win32,_} -> ".bat";
+        {_,_} -> ""
+    end.
