ed4daca5640158ff85c2a6ff9b3c4357f51a96a5
ssbx <ssbx@sysmo.io>
2015-08-17 20:13:14 +0200

    notice modified, license updates

--- src/go/pping.go
+++ src/go/pping.go
@@ -1,3 +1,23 @@
+/*
+ * Sysmo NMS Network Management and Monitoring solution (http://www.sysmo.io)
+ *
+ * Copyright (c) 2012-2015 Sebastien Serre <ssbx@sysmo.io>
+ *
+ * This file is part of Sysmo NMS.
+ *
+ * Sysmo NMS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Sysmo NMS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package main
 
 import "fmt"
